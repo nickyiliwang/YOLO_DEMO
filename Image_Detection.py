a0d82c633161ae08d36fec5f68871f97ddf32c70
nickyiliwang <nickyiliwang@gmail.com>
2022-04-20 16:32:55 -0400

    added more comments about the code, moved input images and video to inputs folder

--- Image_Detection.py
+++ Image_Detection.py
@@ -1,13 +1,19 @@
+# cv2 (opencv) is a library of programming functions mainly aimed at real-time computer vision. 
 import cv2
+# NumPy is a large collection of high-level mathematical functions to operate on these arrays
 import numpy as np
 
+# importing the pre-trained weights file and config file
 net = cv2.dnn.readNet(
     'resources/yolov2-tiny.weights', 'resources/yolov3.cfg')
+# classes variable will contain identified object names (ie. bike, car, bottle, person)
 classes = []
+# reading the predefined names from coco file and assigning it to the classes variable
 with open('resources/coco.names', 'r') as f:
     classes = f.read().splitlines()
 
-img = cv2.imread('resources/rock.jpg') # input file location
+# input file location, input images/videos that you want identified
+img = cv2.imread('resources/inputs/rock.jpg') 
 height, width, _ = img.shape
 
 # prepare the image blob
@@ -22,19 +28,19 @@ blob = cv2.dnn.blobFromImage(
 net.setInput(blob)
 
 # OUTPUTS
+# Returns indexes of layers with unconnected outputs
 output_layers_names = net.getUnconnectedOutLayersNames()
 layerOutputs = net.forward(output_layers_names)
 
-# results are stored in lists
-# bounding box, confidences, prediction class
-boxes = []
-confidences = []
-class_ids = []
+# results
+boxes = [] # bounding box to draw onto the image
+confidences = [] # how confident is the prediction
+class_ids = [] # labeling of the prediction object
 
 # nested forloops for layers outputs, second forloop extracts info in each output
 for output in layerOutputs:
     for detection in output:
-        # get all predictions starting from the 6th element, first 5 are locations and such, unrelated
+        # get all predictions starting from the 6th element, first 5 are locations and such, unrelated data
         scores = detection[5:]
         # get the location of the highest score
         class_id = np.argmax(scores)
@@ -63,15 +69,17 @@ results = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
 font = cv2.FONT_HERSHEY_PLAIN
 colors = np.random.uniform(0, 255, size=(len(boxes), 3))
 
-# checking for null values
+# checking for null values, or empty results
 if len(results) > 0:
     # identify each object
+    # flatten(): [[5, 6], [7, 8]] => [5 6 7 8]
     for i in results.flatten():
         x, y, w, h = boxes[i]
+        # stringify the results
         label = str(classes[class_ids[i]])
         confidence = str(round(confidences[i], 2))
         color = colors[i]
-        # create a rectangle
+        # draw the rectangle
         cv2.rectangle(img, (x, y), (x+w, y+h), color, 2)
         # put output texts back onto the image, upper left
         cv2.putText(img, label + " " + confidence,
